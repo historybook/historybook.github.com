e4aecd0c96fd87ea80fcb88692cfe23876a5f27c
janjakabovic <janyjakabovic@gmail.com>
2013-03-09 21:06:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,11 +12,14 @@ for(i=1;i<subpole.length;i++)
 {obrazky=array();
 obrazky.push(subpole[i]);
 }
-if (subpole<2){document.getElementById("obrazok").src="http://historybook.github.com/bez.png"}
-elseif(subpole<3){document.getElementById("1").style.display="none";document.getElementById("2").style.display="none";document.getElementById("obrazok").src=obrazky[1];}
+if (subpole<2){document.getElementById("obrazok").src="http://historybook.github.com/bez.png";}
+elseif(subpole<3){
+					document.getElementById("1").style.display="none";
+					document.getElementById("2").style.display="none";
+					document.getElementById("obrazok").src=obrazky[1];}
 else{document.getElementById("1").style.display="block";
      document.getElementById("2").style.display="block";
-   document.getElementById("obrazok").src=obrazky[0];
+	 document.getElementById("obrazok").src=obrazky[0];
 	 document.getElementById("1").style.backgroundColor="rgba(0,0,0,0.7)";
 	 document.getElementById("2").style.backgroundColor="rgba(255,255,255,0.7)"
 	 }
